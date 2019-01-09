9002186cc221970fcff006ad0a7fba6aae1d6dce
Evandroverdino <evandroverdino@gmail.com>
2019-01-09 16:42:24 -0300

    Add service to consume client api

---
+++ app/src/main/java/com/evlj/findmovie/network/service/MovieService.kt
@@ -0,0 +1,20 @@
+package com.evlj.findmovie.network.service
+
+import com.evlj.findmovie.model.Discover
+import com.evlj.findmovie.network.api.MovieApi
+import com.evlj.findmovie.network.factory.ServiceFactory
+import io.reactivex.Single
+
+class MovieService : ServiceFactory<MovieApi>(MovieApi::class.java) {
+
+    fun getPopularMovies(
+        apiKey: String, language: String,
+        sortBy: String, includeAdult: Boolean,
+        includeVideo: Boolean, page: Int
+    ): Single<Discover> =
+        movieApi.getPopularMovies(
+            apiKey, language, sortBy,
+            includeAdult, includeVideo, page
+        )
+
+}
