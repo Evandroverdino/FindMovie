26870243fb51e74a4b7ce2dd44ac29c32a2244fc
Evandroverdino <evandroverdino@gmail.com>
2019-01-11 14:56:04 -0300

    Remove unnecessary dialog, following material guidelines

--- app/src/main/java/com/evlj/findmovie/base/BaseActivity.kt
+++ app/src/main/java/com/evlj/findmovie/base/BaseActivity.kt
@@ -1,18 +1,9 @@
 package com.evlj.findmovie.base
 
-import android.app.AlertDialog
 import android.support.v7.app.AppCompatActivity
 import android.widget.Toast
-import com.evlj.findmovie.R
 
 abstract class BaseActivity : AppCompatActivity(), BaseContract {
-
-    private val alertDialog: AlertDialog by lazy {
-        val dialog = AlertDialog.Builder(this)
-        dialog.setMessage(getString(R.string.loading))
-        dialog.setCancelable(true)
-        dialog.create()
-    }
 
     override fun showMessage(messageId: Int) {
         showMessage(getString(messageId))
@@ -22,12 +13,8 @@ abstract class BaseActivity : AppCompatActivity(), BaseContract {
         Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
     }
 
-    override fun showLoading() {
-        alertDialog.show()
-    }
+    override fun showLoading() {}
 
-    override fun dismissLoading() {
-        alertDialog.dismiss()
-    }
+    override fun dismissLoading() {}
 
 }
