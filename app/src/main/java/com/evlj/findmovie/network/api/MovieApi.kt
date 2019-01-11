a668be14b0ac565dfbf9ddcf25b582c77f08d14d
Evandroverdino <evandroverdino@gmail.com>
2019-01-11 00:28:05 -0300

    Remove wrong annotation from client api method

--- app/src/main/java/com/evlj/findmovie/network/api/MovieApi.kt
+++ app/src/main/java/com/evlj/findmovie/network/api/MovieApi.kt
@@ -2,13 +2,11 @@ package com.evlj.findmovie.network.api
 
 import com.evlj.findmovie.model.Discover
 import io.reactivex.Single
-import retrofit2.http.FormUrlEncoded
 import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface MovieApi {
 
-    @FormUrlEncoded
     @GET("discover/movie")
     fun getPopularMovies(
         @Query("api_key") apiKey: String,
