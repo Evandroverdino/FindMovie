dfb977a50c6c275683fa5ae646c6f3af1392918b
Evandroverdino <evandroverdino@gmail.com>
2020-06-21 13:25:39 -0300

    Remove misplaced api url from presentation layer

--- presentation/src/main/java/com/evlj/findmovie/shared/Constants.kt
+++ presentation/src/main/java/com/evlj/findmovie/shared/Constants.kt
@@ -2,7 +2,6 @@ package com.evlj.findmovie.shared
 
 object Constants {
 
-    const val API_BASE_URL = "https://api.themoviedb.org/3/"
     const val API_POSTER_URL = "https://image.tmdb.org/t/p/"
 
     const val API_KEY = "de3c5b5353233b1d09b6e163b5df2c99"
