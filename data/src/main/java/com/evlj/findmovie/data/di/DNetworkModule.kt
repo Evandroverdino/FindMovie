22e9936145cd6be7f66ebc0dbdab9404057b465e
Evandroverdino <evandroverdino@gmail.com>
2020-06-29 22:14:40 -0300

    Remove rx adapter instance from retrofit builder

--- data/src/main/java/com/evlj/findmovie/data/di/DNetworkModule.kt
+++ data/src/main/java/com/evlj/findmovie/data/di/DNetworkModule.kt
@@ -11,7 +11,6 @@ import okhttp3.logging.HttpLoggingInterceptor
 import org.koin.core.qualifier.named
 import org.koin.dsl.module
 import retrofit2.Retrofit
-import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
 import java.util.concurrent.TimeUnit
 
@@ -35,7 +34,6 @@ val dataNetworkModule = module {
             .baseUrl(Constants.API_BASE_URL)
             .client(get())
             .addConverterFactory(GsonConverterFactory.create(get()))
-            .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
             .build()
     }
     single { RetrofitFactory.create<MovieApi>(get()) }
